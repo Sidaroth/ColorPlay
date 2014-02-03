2df24266369d8852e9f36e75a032297d495e4dd2
ar7ic <henrik_hlj@hotmail.com>
2014-02-03 17:19:32 +0100

    working compilation

--- ColorPlay/main.cpp
+++ ColorPlay/main.cpp
@@ -7,14 +7,63 @@
 #include <curl/curl.h>
 
 #define DEBUG 1
+#define CURL_STATICLIB
 
 void debugMessage(std::string message, int debugLevel);
 void colorCalibration(int low = 0, int high = 65535, int step = 10, int stepDelay = 0);
 
+void command(CURL *curl, char *body, int bulb){
+
+	CURLcode res;
+	struct curl_slist *headers = NULL;
+
+
+
+	curl = curl_easy_init();
+
+	if (curl) {
+		printf("sending request \n");
+
+
+		char* json_struct = body;
+		char bulb1[] = "http://192.168.1.2/api/newdeveloper/lights/1/state";
+		char bulb2[] = "http://192.168.1.2/api/newdeveloper/lights/2/state";
+		char bulb3[] = "http://192.168.1.2/api/newdeveloper/lights/3/state";
+
+		headers = curl_slist_append(headers, "Accept: application/json");
+		headers = curl_slist_append(headers, "Content-Type: application/json");
+		headers = curl_slist_append(headers, "charsets: utf-8");
+
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		if (bulb == 1)
+			curl_easy_setopt(curl, CURLOPT_URL, bulb1);
+		else if (bulb == 2)
+			curl_easy_setopt(curl, CURLOPT_URL, bulb2);
+		else if (bulb == 3)
+			curl_easy_setopt(curl, CURLOPT_URL, bulb3);
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_struct);
+
+		res = curl_easy_perform(curl);
+
+		curl_slist_free_all(headers);
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+
+}
+
 int main(int argc, char* argv[])
 {
-	colorCalibration();
-	_getch();
+	//colorCalibration();
+	//_getch();
+	CURL *curl;
+
+	curl = curl_easy_init();
+
+	command(curl, "{\"on\":false}", 1);
+
 	return 0;	
 }
 

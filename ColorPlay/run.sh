433bd935ba99100fb4a5504db31aef7005b8ea4e
johhov <johannes.hovland@gmail.com>
2014-02-10 22:42:35 +0100

    Added .exe and .orig to .gitignore and fixed linking for the ps move

--- ColorPlay/run.sh
+++ ColorPlay/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=../lib/poco/lib 	# Force the dynamic linker to check this path for poco.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib/SFML/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib/psmoveapi/build
 echo "Adding library search path(s): $LD_LIBRARY_PATH"
 
 ./ColorPlay.exe

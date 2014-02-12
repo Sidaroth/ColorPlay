52ed7fba21e12bef74717dd0a94f8ea5ba89bd15
ar7ic <ar7ic@ubuntu.(none)>
2014-02-12 13:58:39 +0100

    Started lab2xyz

---
+++ ColorPlay/BulbMath.hpp
@@ -0,0 +1,15 @@
+// Header file for BulbMath used for color conversion 
+
+#include <iostream>
+
+class BulbMath
+{
+public: 
+	BulbMath();
+	float lab2xyz(float x, float y, float z);
+
+private:
+
+
+
+};
--- ColorPlay/main.cpp
+++ ColorPlay/main.cpp
@@ -6,6 +6,7 @@
 
 
 #include "BulbHandler.hpp"
+#include "BulbMath.hpp"
 #include "LogModule.hpp"
 #include "StringQueue.hpp"
 #include "MoveHandler.hpp"
@@ -16,6 +17,7 @@
 int main(int argc, char* argv[])
 {
 	BulbHandler bulbHandler;
+	BulbMath bulbMath;
 	LogModule logger;
 	MoveHandler moveHandler(&logger);
 	WindowHandler windowHandler("Color Play Game v.0.1", &logger);
@@ -32,6 +34,7 @@ int main(int argc, char* argv[])
 	logger.LogEvent("Adding lightBulb 3");
 	logger.LogEvent("Adding lightBulb 4");
 	
+	bulbMath.lab2xyz(1,1,1);
 
 	///////////////// START THREADS /////////////////
 	std::thread loggerThread(&LogModule::run, &logger);	// Run the logger module in a background thread.

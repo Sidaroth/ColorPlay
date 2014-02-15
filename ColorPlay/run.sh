7c58fc7667d2a3a01eb03a7be0c8b6a95de63790
Christian Holt <ymabob@gmail.com>
2014-02-15 23:19:51 +0100

    Fix run script

--- ColorPlay/run.sh
+++ ColorPlay/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo LD_LIBRARY_PATH=../lib/poco/lib 	# Force the dynamic linker to check this path for poco.
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib/SFML/lib
-sudo LD_LIBRARY_PATH:=$LD_LIBRARY_PATH:../lib/psmoveapi/build
-echo "Adding library search path(s): $LD_LIBRARY_PATH"
+sudo LD_LIBRARY_PATH=../lib/SFML/lib:../lib/psmoveapi/build:../lib/curl/lib ./ColorPlay.exe # Force the dynamic linker to check these paths for libs.
 
-./ColorPlay.exe
+#sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib/SFML/lib
+#sudo LD_LIBRARY_PATH:=$LD_LIBRARY_PATH:../lib/psmoveapi/build
+echo "Adding library search path(s): $LD_LIBRARY_PATH"

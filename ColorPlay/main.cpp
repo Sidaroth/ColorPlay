effa34b2a93b53c806fd195eb9745b7eccadc490
Christian Holt <ymabob@gmail.com>
2014-02-20 01:02:48 +0100

    Add preliminary support for changing ColorSpaces dynamically

--- ColorPlay/main.cpp
+++ ColorPlay/main.cpp
@@ -83,7 +83,6 @@ int main(int argc, char* argv[])
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	bulbHandler.setColorSpace(BulbHandler::ColorSpace::HSV);
-	bulbHandler.updateTargetBulb();
 
 	while(running)
 	{
